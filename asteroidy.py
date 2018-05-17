281589495129231dc991a6062d2b21d059361f89
Skurkys <31956765+Skurkys@users.noreply.github.com>
2018-05-17 12:36:54 +0200

    Add files via upload

--- asteroidy.py
+++ asteroidy.py
@@ -8,6 +8,86 @@ Created on Thu Apr 26 13:26:10 2018
 
 import pyglet
 
+from pyglet.window.key import MOD_SHIFT, MOD_CTRL, DOWN, UP, LEFT, RIGHT
+from pyglet.window.mouse import LEFT as mLEFT
+from pyglet.window.mouse import RIGHT as mRIGHT
+from math import sin, cos, pi
+
 window = pyglet.window.Window()
+
+obrazek = pyglet.image.load('obrazek.png')
+obrazek.anchor_x = obrazek.height // 2
+obrazek.anchor_x = obrazek.width // 2
+sprite = pyglet.sprite.Sprite(obrazek)
+
+uhel = 60
+sprite.rotation = 60
+rychlost = 30
+klavesy = ()
+
+
+def tiktak(t):
+    global rychlost
+    global uhel
+    
+    #posun
+    sprite.x = sprite.x + rychlost*t*sin(pi*uhel/180)
+    sprite.y = sprite.y + rychlost*t*cos(pi*uhel/180)
+
+    #natočení
+    for sym in klavesy:
+        if sym == DOWN:
+            if rychlost < 10:
+                rychlost = 0
+        else:
+            rychlost -= 10
+            if sym == UP:
+                if rychlost > 90:
+                    rychlost = 100
+                rychlost += 10
+            if sym == LEFT:
+                uhel -=10
+                sprite.rotation -=10
+            if sym == RIGHT:
+                uhel +=10
+                sprite.rotation +=10
+
+@window.event
+def on_draw():
+    window.clear()
+    sprite.draw()
+
+
+@window.event
+def on_mouse_press(x, y, button, mod):
+    global uhel
+    if button == mLEFT:
+        sprite.x = x
+        sprite.y = y
+    elif button == mRIGHT and (mod & MOD_SHIFT):
+        sprite.rotation += 180
+        uhel += 180
+    elif button == mRIGHT and (mod & MOD_CTRL):
+        sprite.rotation += 90
+        uhel += 90
+    elif button == mRIGHT:
+        sprite.rotation += 10
+        uhel += 10
+
+@window.event
+def on_key_press(sym, mod):
+    global klavesy
+    klavesy.add(sym)
+    
+
+@window.event
+def on_key_release(sym, mod):
+    global klavesy
+    klavesy.add(sym)
+
+
+pyglet.clock.schedule_interval(tiktak, 1/25)
+
 pyglet.app.run()
 print('Hotovo!')
+
